76bcd21e3e728df21fc5cd137caff916585b77fd
Minttu <K8517@student.jamk.fi>
2018-01-24 11:33:46 +0200

    update

--- viikko4/state6.js
+++ viikko4/state6.js
@@ -3,7 +3,7 @@ demo.state6.prototype = {
     preload: function(){},
     create: function(){
         game.stage.backgroundColor = '#cc0066';
-        
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         addChangeStateEventListeners();
     },
     update: function(){}
